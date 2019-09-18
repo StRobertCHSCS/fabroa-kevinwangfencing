82b612bfa8f243c5eeb17a3b84ec5fb3bfc3e666
kevinwangfencing <kevin.wang22@ycdsbk12.ca>
2019-09-18 08:54:34 -0400

    print examples

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,11 @@
+print("This is a print statement")
+print ("What's wrong with this statement?")
+print ("print How about this one?") 
+print (5)
+print (3.14)
+print ("Here are the ages of my three children: ", 12, 7, 4)
+greeting = "Hello world!"
+print ("greeting")
+length = 16
+width = 20
+print("The dimensions of the yard are", length,"by", width)
