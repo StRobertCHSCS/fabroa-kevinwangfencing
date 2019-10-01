bd500f38dde600f301ef32840c7ccdae21feee72
kevinwangfencing <kevin.wang22@ycdsbk12.ca>
2019-10-01 09:49:36 -0400

    submit practice

---
+++ Working/LiveHacks/LiveHack1/practice1.py
@@ -0,0 +1,8 @@
+# get fahrenheit from user
+fahrenheit = float (input("Enter the fahrenheit:"))
+
+# compute celsius from fahrenheit
+celsius = (5/9) * (fahrenheit - 32)
+
+# output celsius
+print ("The temperature in celsius is " + str(celsius))
