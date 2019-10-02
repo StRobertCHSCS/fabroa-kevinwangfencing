0183e01721e2d55a0b3621633efc9134969469a5
kevinwangfencing <kevin.wang22@ycdsbk12.ca>
2019-10-02 09:47:42 -0400

    Problem 1 & problem 2 - Kevin

---
+++ Working/LiveHacks/LiveHack1/problem1.py
@@ -0,0 +1,8 @@
+#1 get Celsius from user
+Celsius = float (input("Enter the Celsius:"))
+
+#2 compute Fehrenheit to Celsius
+Fehrenheit = (9/5) * (Celsius + 32)
+
+#3 output Fahrenheit
+print ("The temperture in Fahrenheit is " + str(Fahrenheit))
---
+++ Working/LiveHacks/LiveHack1/problem2.py
@@ -0,0 +1,9 @@
+# numbers of chicken in total
+chicken in total = float (input("Enter the total:"))
+
+#numbers of students distribute
+float (input("Enter chicken in total")) = 32
+float (input("Enter students distribute")) = 20
+
+#output Mr.Fabroa recieved
+print ("Mr.Fabroa will receieve" = str(32-20)
